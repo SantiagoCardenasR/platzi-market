e73ff618bcc923e87b533440d23464c10dd5f4d0
SantiagoCardenasR <30352306+SantiagoCardenasR@users.noreply.github.com>
2021-06-04 14:56:45 -0500

    Cambios pequeños para subir a Heroku

--- src/main/java/com/Platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/Platzi/market/web/security/SecurityConfig.java
@@ -28,15 +28,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
-                "/swagger-resources/**", "/configuration/security",
-                "/swagger-ui.html", "/webjars/**");
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate",
+                "/v2/api-docs", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
+                "/configuration/security", "/swagger-ui.html", "/webjars/**",
+                // -- Swagger UI v3 (OpenAPI)
+                "/v3/api-docs/**",
+                "/swagger-ui/**").permitAll()
                 .anyRequest().authenticated().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
